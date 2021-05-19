e175a47bbb7d4e3bc05c046519c7bc2cb00e21e6
StanleySenne <56733982+StanleySenne@users.noreply.github.com>
2021-05-18 21:47:46 -0800

    Add files via upload

---
+++ byron/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>byron.solutions</title>
+		
+		<!-- Fonte -->
+
+		<link href='https://fonts.googleapis.com/css?family=Alegreya+SC' rel='stylesheet' type='text/css'>
+
+		<!-- Bootstrap -->
+
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/style.css">
+
+		<!-- Scripts -->
+
+		<script
+  	src="https://code.jquery.com/jquery-3.6.0.min.js"
+  	integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  	crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+		<!-- Font Awesome -->
+
+		<script src="https://kit.fontawesome.com/b73f499498.js" crossorigin="anonymous"></script>
+
+		<!-- Progress bar -->
+
+		<script src="js/progressbar.min.js"></script>
+
+		<!-- Parallax -->
+
+		<script src="https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js"></script>
+
+	</head>
+
+	<body>
+		<header>
+			<nav>
+				<ul>
+					<li class="current_page_item"><a href="index.html">Home</a></li>
+					<li><a href="Portfolio.html">Portfólio</a></li>
+					<li><a href="Blog.html">Blog</a></li>
+					<li><a href="Sobre.html">Quem Somos</a></li>
+				</ul>
+			</nav>
+
+			<h1><img src="<?php echo get_stylesheet_directory_uri() ?>/img/logo_byron.png" alt="byron"></h1>
+		</header>
+
+		<script src="js/scripts.js"></script>
+	</body>
+</html>
